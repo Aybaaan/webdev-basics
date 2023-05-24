f41fe0cff99c1d795407ebc15e7dd1185d3ca240
Ivan Veluz <johnivanveluz@gmail.com>
2023-05-25 00:02:04 +0800

    added project 8

---
+++ js/proj8_app.js
@@ -0,0 +1,14 @@
+const containerEl = document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
+const popupContainerEL = document.querySelector(".popup-container");
+const closeIconEl = document.querySelector(".close-icon");
+
+btnEl.addEventListener("click", () => {
+    containerEl.classList.add("active");
+    popupContainerEL.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", () => {
+    containerEl.classList.remove("active");
+    popupContainerEL.classList.add("active");
+});
