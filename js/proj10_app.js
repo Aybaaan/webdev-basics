a155e0bc74d3bcb9c049830833e47d617cc85017
Ivan Veluz <johnivanveluz@gmail.com>
2023-05-26 00:05:39 +0800

    added projects 9, 10, and 11
    - added projects 9, 10, and 11
    - fixed filenames

---
+++ js/proj10_app.js
@@ -0,0 +1,36 @@
+const nextEl = document.querySelector(".next");
+const prevEl = document.querySelector(".prev");
+const imgContainerEl = document.querySelector(".image-container");
+const imgsEl = document.querySelectorAll("img");
+
+let currImg = 1;
+
+let timeout;
+
+nextEl.addEventListener("click", ()=>{
+    currImg++;
+    clearTimeout(timeout);
+    updateImg();
+});
+
+prevEl.addEventListener("click", () =>{
+    currImg--;
+    clearTimeout(timeout);
+    updateImg();
+})
+
+updateImg();
+
+function updateImg(){
+    if(currImg > imgsEl.length){
+        currImg = 1;
+    } else if(currImg < 1){
+        currImg = imgsEl.length;
+    }
+    imgContainerEl.style.transform = `translateX(-${(currImg - 1) * 500}px)`;
+
+    timeout = setTimeout(()=>{
+        currImg++;
+        updateImg();
+    }, 3000);
+}
